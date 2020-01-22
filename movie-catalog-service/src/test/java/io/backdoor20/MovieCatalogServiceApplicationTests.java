a17fffb0314ae6fb239a51539ccd4660031348f7
bgadwal <bgadwal@saba.com>
2020-01-22 18:46:12 +0530

    Creating a spring boot project for movie-catalog service

---
+++ movie-catalog-service/src/test/java/io/backdoor20/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.backdoor20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
