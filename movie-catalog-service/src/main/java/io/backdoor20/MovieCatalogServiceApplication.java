cb6b2f490273ee51d68b3a17a73df03f49003488
bgadwal <bgadwal@saba.com>
2020-01-23 13:21:21 +0530

    Making restCall using web client using webflux

--- movie-catalog-service/src/main/java/io/backdoor20/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/backdoor20/MovieCatalogServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
@@ -13,6 +15,10 @@ public class MovieCatalogServiceApplication {
 		return new RestTemplate();
 	}
 	
+	@Bean
+	public WebClient.Builder getWebClientBuilder(){
+		return WebClient.builder();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
 	}
--- movie-catalog-service/src/main/java/io/backdoor20/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/backdoor20/resources/MovieCatalogResource.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import io.backdoor20.models.CatalogItem;
 import io.backdoor20.models.Movie;
@@ -23,13 +24,26 @@ public class MovieCatalogResource {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable String userId){
 		List<Rating> ratings=Arrays.asList(new Rating("1234", 4),
 				new Rating("5678", 3));
 		
 		return ratings.stream().map(rating -> {
-			Movie movie=restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+			//Movie movie=restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+			
+			Movie movie=webClientBuilder.build()
+			.get()
+			.uri("http://localhost:8082/movies/"+rating.getMovieId())
+			.retrieve()
+			.bodyToMono(Movie.class)
+			.block();
+		
+			
 			return new CatalogItem(movie.getMovieName(), "Description for Movie", rating.getRating());
 		}).collect(Collectors.toList());
 		//return Collections.singletonList(new CatalogItem("Pk", "Movie by amir khan", 4));
